f61998564ab00d5bb6e73dbe841cef8a942879f6
georgepiter <george.piter@terra.com.br>
2021-01-16 01:51:33 -0300

    Initial Commit

---
+++ back/src/test/java/com/entrega/veloz/EntregavelozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entrega.veloz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregavelozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
