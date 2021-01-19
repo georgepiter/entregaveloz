80c7f0b4f124567e19ed8fca74d37125d1d31f64
georgepiter <george.piter@terra.com.br>
2021-01-19 19:55:20 -0300

    Data URL

--- front/src/app/services/veloz.service.ts
+++ front/src/app/services/veloz.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+const url = process.env.API_URL;
 
 @Injectable({
   providedIn: 'root'
 })
-export class VelozService {
-
-  private url: string = 'http://localhost:8091';
-
+export class VelozService {  
+    
   constructor(private http: HttpClient) { }
 
   getEncomendaRastreio(codigo: string): Observable<any> {
-    return this.http.get(`${this.url}/pedido/${codigo}`);
+    return this.http.get(`${url}/pedido/${codigo}`);
   }
 
 }
