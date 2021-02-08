ac4da0b45d660325c36f416a9b774a3c8e27e480
georgepiter <george.piter@terra.com.br>
2021-02-08 17:06:25 -0300

    Ajuste no environments-prod"

--- front/src/app/services/veloz.service.ts
+++ front/src/app/services/veloz.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
 
-const url = URL
 @Injectable({
   providedIn: 'root'
 })
 export class VelozService {  
-    
+
+   private API_URL = environment.API_URL;  
   constructor(private http: HttpClient) { }
 
   getEncomendaRastreio(codigo: string): Observable<any> {
-    return this.http.get(`${url}/pedido/${codigo}`);
+    return this.http.get(this.API_URL + `pedido/`+ codigo);
   }
-
 }
+
+
--- front/src/environments/environment.prod.ts
+++ front/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL
+  API_URL: URL
 };
 
 
